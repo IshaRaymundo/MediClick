c483fc9642816be472de5830c796c74d8db5d296
LaraUT <133019709+LaraUT@users.noreply.github.com>
2024-11-26 17:15:27 -0600

    modificacion al metodo getallcitas del modelo

--- backend/models/Cita.js
+++ backend/models/Cita.js
@@ -21,26 +21,24 @@ static async getAllCitas() {
     await checkConnection();
     const connection = await pool.getConnection();
     try {
-        // Consulta para incluir el nombre del doctor y del paciente
         const [result] = await connection.execute(`
             SELECT 
-                citas.id,
+                citas.id AS citaId,
                 citas.doctor_id,
-                citas.user_id,  -- Esto es el paciente (usuario)
+                citas.user_id,
                 citas.fecha,
                 citas.hora_inicio,
                 citas.hora_fin,
                 citas.estado_id,
-                doctores.id AS doctor_id,
+                horarios_ocupados.id AS disponibilidad_id,
                 usuarios.username AS doctor_nombre,
-                pacientes.username AS paciente_nombre,  -- Agregar el nombre del paciente
-                GROUP_CONCAT(especialidades.nombre SEPARATOR ', ') AS especialidades
+                GROUP_CONCAT(DISTINCT especialidades.nombre SEPARATOR ', ') AS especialidades
             FROM citas
             JOIN doctores ON citas.doctor_id = doctores.id
             JOIN users AS usuarios ON doctores.user_id = usuarios.id
-            JOIN users AS pacientes ON citas.user_id = pacientes.id  -- Aquí unimos la tabla de usuarios para obtener el nombre del paciente
             LEFT JOIN doctor_especialidad ON doctores.id = doctor_especialidad.doctor_id
             LEFT JOIN especialidades ON doctor_especialidad.especialidad_id = especialidades.id
+            LEFT JOIN horarios_ocupados ON horarios_ocupados.disponibilidad_id = citas.doctor_id
             GROUP BY citas.id
         `);
 
@@ -49,6 +47,8 @@ static async getAllCitas() {
         connection.release();
     }
 }
+
+
 
 
     // Actualizar el estado de una cita
